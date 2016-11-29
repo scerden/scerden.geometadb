43f8b2bf1d1c9b28c13341f4b14dfb3745e9ff78
Nicolas PJ Coutin <nicolas.coutin@gmail.com>
2016-11-28 17:30:51 -0800

    remove redudant observations in data/*.rda

--- data-raw/03_save-data-objects.R
+++ data-raw/03_save-data-objects.R
@@ -10,6 +10,6 @@ files <- map(pattern, ~str_subset(tbls, .x))
 stopifnot(all(unlist(lapply(files, length) == 1))) # make sure each matches only 1 file
 
 # assign objects
-walk2(obj_name, files, ~ assign(.x, read_csv(.y), envir = .GlobalEnv))
+walk2(obj_name, files, ~ assign(.x, read_csv(.y) %>% unique(), envir = .GlobalEnv))
 
 use_data(`gds-subset`, gds, gpl, gse, gsm)
